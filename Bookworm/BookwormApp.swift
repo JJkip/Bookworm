4f24afb5ce75ecc556197f13675639d9359c61f3
jjkip <jjlangatjose@gmail.com>
2023-07-06 23:13:22 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Joseph Langat on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
