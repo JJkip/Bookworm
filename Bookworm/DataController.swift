d09b03b47ab7ba599d242da90b7f83d4955dcb0b
jjkip <jjlangatjose@gmail.com>
2023-07-13 22:50:49 +0300

    Creating an instance of DataController and send it into ContentView() SwiftUI’s environment

--- Bookworm/DataController.swift
+++ Bookworm/DataController.swift
@@ -9,4 +9,13 @@ import Foundation
 
 class DataConroller: ObservableObject {
     let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+            
+        }
+    }
 }
