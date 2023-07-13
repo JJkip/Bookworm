b886e3550d5b5d4bdb03b9f234f8a6cb3aa78049
jjkip <jjlangatjose@gmail.com>
2023-07-13 23:47:16 +0300

    How to combine Core Data and SwiftUI

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -32,7 +32,29 @@ struct ContentView: View {
     @State private var rememberMe = false
     @AppStorage("notes") private var notes = ""
     
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     var body: some View {
+        VStack {
+            List(students) { student in
+                Text(student.name ?? "Unknown")
+                
+            }
+            Button("Add") {
+                let firstNames = ["Petero","Lilly","Gosy","Cosmas","Koemn","Erick","Mark"]
+                let lastNames = ["Kiptoo","Wairimu","Atieno","Mukaya","Mueni","Bosek","Boit"]
+                
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+            }
+        }
+        /*
         NavigationView {
             VStack {
                 TextEditor(text: $notes)
@@ -42,6 +64,7 @@ struct ContentView: View {
                 Text(rememberMe ? "On" : "off")
             }
         }
+        */
     }
 }
 
