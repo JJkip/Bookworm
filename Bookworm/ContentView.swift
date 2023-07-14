384c4c67dce108ee60b1e08b7a7027dd9344a8d7
jjkip <jjlangatjose@gmail.com>
2023-07-14 23:07:04 +0300

    Add book form added and Book entry added

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -7,64 +7,15 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.purple, Color.pink]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(
-            LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom)
-        )
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius:  isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
-    @State private var rememberMe = false
-    @AppStorage("notes") private var notes = ""
-    
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     var body: some View {
         VStack {
-            List(students) { student in
-                Text(student.name ?? "Unknown")
-                
-            }
-            Button("Add") {
-                let firstNames = ["Petero","Lilly","Gosy","Cosmas","Koemn","Erick","Mark"]
-                let lastNames = ["Kiptoo","Wairimu","Atieno","Mukaya","Mueni","Bosek","Boit"]
-                
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastNames.randomElement()!
-                
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? moc.save()
-            }
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
-        /*
-        NavigationView {
-            VStack {
-                TextEditor(text: $notes)
-                    .navigationTitle("Notes")
-                    .padding()
-                PushButton(title: "Remember Me", isOn: $rememberMe)
-                Text(rememberMe ? "On" : "off")
-            }
-        }
-        */
+        .padding()
     }
 }
 
