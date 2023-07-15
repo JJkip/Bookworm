25fb00bc34561be2e89a1e36ce455b37e5b79536
jjkip <jjlangatjose@gmail.com>
2023-07-15 23:03:37 +0300

    Adding a custom star rating component

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -21,7 +21,27 @@ struct RatingView: View {
     var onColor = Color.yellow
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+                    .onTapGesture {
+                        rating = number
+                    }
+                
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
     }
 }
 
