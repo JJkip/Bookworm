ee5cc131806645553d901259208f1ac5d979a6db
jjkip <jjlangatjose@gmail.com>
2023-07-17 21:47:28 +0300

    Deleting from a Core Data fetch request

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -36,9 +36,13 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading){
+                    EditButton()
+                }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -51,6 +55,15 @@ struct ContentView: View {
                 AddBookView()
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        /*This will delete the saved book*/
+//        try? moc.save()
     }
 }
 
